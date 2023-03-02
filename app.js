d2ec00c7e7cbf731049cbd011d05d84b5e2c3290
Bruce <nbruce420@gmail.com>
2023-03-02 20:09:17 +0200

    second commit

--- app.js
+++ app.js
@@ -10,60 +10,52 @@ const dbURI =
 
 mongoose
     .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(3000))
+    .then(() => {
+        app.listen(3000);
+        console.log("mongodb connected successfully");
+    })
     .catch((err) => console.log(err));
 
 //register view engine
 app.set("view engine", "ejs");
 
-app.use(express.static("./public"));
-app.use(morgan("tiny"));
+//middlewares
+app.use(express.static("./public/"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-//mongoose and mongo sandbox
-app.get("/add-blog", (req, res) => {
-    const blog = new Blog({
-        title: "new blog 2",
-        snippet: "about my new blog",
-        body: "more about my new blog",
-    });
-    blog.save()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-});
-
-app.get("/all-blogs", (req, res) => {
-    Blog.find()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-});
-
+//routes
 app.get("/", (req, res) => {
-    const blogs = [
-        {
-            title: "Yoshi finds eggs",
-
-            snippet:
-                "De Facto standards: A format, or protocol that has become a standard not because it has been approved by a standards organization but because it is widely used in networking",
-        },
-        {
-            title: "Mario finds starts",
-            snippet:
-                " Standards Organization => ISO: Short for International Organization for Standardization. Note that ISO is not an acronym; instead, the name derives from the Greek word iso, which means equal",
-        },
-        {
-            title: "How to defeat bowser",
-            snippet:
-                "IEEE: The INstiture of Electrical and Electronics Engineers Standards Association(IEEE-SA) is an organization within IEEE that develops global standards in a broad range of industries, including power and energy,",
-        },
-    ];
-    res.render("index", { title: "Home", blogs });
+    res.redirect("/blogs");
 });
+
+//dynamic routes
+app.get("/blogs", (req, res) => {
+    Blog.find()
+        .sort({ createdAt: -1 })
+        .then((result) => {
+            res.render("index", { title: "All Blogs", blogs: result });
+        })
+        .catch((err) => console.log(err));
+});
+
+app.post("/blogs", (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => res.redirect("/blogs"))
+        .catch((err) => console.log(err));
+});
+
+
+
 app.get("/about", (req, res) => {
     res.render("about", { title: "About" });
 });
-app.get("/blogs/create", (req, res) => {
+
+app.get("/create", (req, res) => {
     res.render("create", { title: "Create a new blog" });
 });
+
 app.use((req, res) => {
     res.status(404).render("404", { title: "404" });
 });
